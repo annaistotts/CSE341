af4faf41164c76df316e70788111876564e8be7f
annaistotts <ins17001@byui.edu>
2025-09-20 17:56:00 -0500

    extra for 1st assignment

---
+++ controllers/lesson1.js
@@ -0,0 +1,12 @@
+const annaRoute = (req, res) => {
+  res.send("Anna Inskeep");
+};
+
+const ethanRoute = (req, res) => {
+  res.send("Ethan");
+};
+
+module.exports = {
+    annaRoute,
+    ethanRoute,
+};
