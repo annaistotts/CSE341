a079dc39f352a2dc06fcb41e4413af974a9fcc4b
annaistotts <ins17001@byui.edu>
2025-09-20 18:07:53 -0500

    changed ethan to home page

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const routes = require('express').Router();
 const lesson1Controller = require('../controllers/lesson1');
  
-routes.get('/', lesson1Controller.annaRoute);
-routes.get('/ethan', lesson1Controller.ethanRoute);
+routes.get('/anna', lesson1Controller.annaRoute);
+routes.get('/', lesson1Controller.ethanRoute);
 
 module.exports = routes;
